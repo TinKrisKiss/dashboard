ee657374ce6e682da16ae493dacba60ff10f7a20
TinKrisKiss <z-qksi@yandex.ru>
2026-06-29 20:00:28 +0500

    что-то

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/dashboard/',
   plugins: [
     react(),
-    tailwindcss(), // подключаем Tailwind как плагин Vite
+    tailwindcss(),
   ],
 })
